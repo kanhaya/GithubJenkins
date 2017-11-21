68182d3d32db712e6d99aac05347b4c81666adea
kanhaya <kanhaya.yadav@mettl.com>
2017-11-21 17:00:06 +0530

    First commit

---
+++ src/test/java/com/GithubJenkins/MyGitHub.java
@@ -0,0 +1,14 @@
+package com.GithubJenkins;
+
+import org.testng.annotations.Test;
+
+public class MyGitHub {
+
+    @Test
+    public void MyTest() {
+        System.out.println("james");
+
+
+        System.out.println("new line");
+    }
+}
